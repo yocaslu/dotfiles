bab6abcf63c91f41c3502c20af47464ac8e3d7c3
olaLucas <lsantos2003silva@gmail.com>
2024-10-16 05:41:54 -0300

    disabling tmux-dev.sh

--- home/bashrc/shell.sh
+++ home/bashrc/shell.sh
@@ -1,6 +1,2 @@
-
-alias xmmp="~/.xmmp.sh"
-alias syu="sudo pacman -Syu"
-
 SHELL_SCRIPTS_PATH=$REPO_PATH/dotfiles/scripts/sh_scripts
-alias dev="exec $SHELL_SCRIPTS_PATH/tmux-dev.sh ~/git-repos/"
+# alias dev="exec $SHELL_SCRIPTS_PATH/tmux-dev.sh ~/git-repos/"
