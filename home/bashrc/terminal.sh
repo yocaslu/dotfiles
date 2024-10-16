2c1cb67f43e48fa8283523a0c615bbda65cdc3d2
olaLucas <lsantos2003silva@gmail.com>
2024-10-16 05:41:59 -0300

    Update terminal.sh

--- home/bashrc/terminal.sh
+++ home/bashrc/terminal.sh
@@ -1,6 +1,9 @@
+alias rc='source ~/.bashrc'
 alias cl='clear'
 alias ll='lsd -lha'
 alias ls='lsd --color=auto'
 alias grep='grep --color=auto'
 alias fetch='fastfetch -l arch2'
 alias tl="tree -lhagpf"
+alias xmmp="~/.xmmp.sh"
+alias syu="sudo pacman -Syu"
