2ddf9c5ba7beb4a400d8faa817467cba30fb9818
olaLucas <lsantos2003silva@gmail.com>
2024-10-21 00:48:38 -0300

    Update shell.sh

--- home/bashrc/shell.sh
+++ home/bashrc/shell.sh
@@ -1,2 +1,2 @@
 SHELL_SCRIPTS_PATH=$REPO_PATH/dotfiles/scripts/sh_scripts
-# alias dev="exec $SHELL_SCRIPTS_PATH/tmux-dev.sh ~/git-repos/"
+alias dev="exec $SHELL_SCRIPTS_PATH/tmux-dev.sh ~/git-repos/"
