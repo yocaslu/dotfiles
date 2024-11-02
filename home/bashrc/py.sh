00fadceb5d9483672ab5994d958c94a120981093
olaLucas <lsantos2003silva@gmail.com>
2024-11-02 08:53:17 -0300

    modified:   config/alacritty/alacritty.toml 	modified:   config/i3/config 	modified:   config/polybar/config.ini 	modified:   home/.ssh/config 	modified:   home/bashrc/py.sh

--- home/bashrc/py.sh
+++ home/bashrc/py.sh
@@ -3,5 +3,7 @@ alias py="python3"
 PY_SCRIPTS_PATH=$REPO_PATH/dotfiles/scripts/py_scripts
 alias pyenv='python3 $PY_SCRIPTS_PATH/pyenv.py $(pwd) && source .env/bin/activate'
 
+alias pp='poetry'
+
 # pipx completions
 # eval "$(register-python-argcomplete pipx)"
