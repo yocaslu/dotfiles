6ad70de488cd23605af9956048ea582ffb54c8d3
olaLucas <lsantos2003silva@gmail.com>
2024-11-10 23:32:14 -0300

    fixing the symlink trying to link to home/config path

--- src/setup/install.rs
+++ src/setup/install.rs
@@ -16,14 +16,24 @@ pub fn link_home(dotfiles_path: &str) {
 
   let dotfiles_home_content = scandir(&dotfiles_home_path);
   for file in dotfiles_home_content.files().iter() {
-    match symlink(file, &home_path) {
+    let file_name = Path::new(file).file_name().unwrap();   
+    let dest = String::from(
+      home_path.to_owned() + "/" + file_name.to_str().unwrap()
+    );   
+    
+    match symlink(file, &dest) {
       Ok(_) => info!("file {} succefully linked to {}", file, home_path),
       Err(e) => error!("failed to link file {} to {}, due to: {}", file, home_path, e) 
     };
   }
 
   for dir in dotfiles_home_content.dirs().iter() {
-    match symlink(dir, &home_path) {
+    let file_name = Path::new(dir).file_name().unwrap();
+    let dest = String::from(
+      home_path.to_owned() + "/" + file_name.to_str().unwrap()
+    );
+
+    match symlink(dir, &dest) {
       Ok(_) => info!("dir {} succefully linked to {}", dir, home_path),
       Err(e) => error!("failed to link dir {} to {}, due to: {}", dir, home_path, e.to_string())
     }
@@ -46,14 +56,24 @@ pub fn link_config(dotfile_path: &str) {
 
   let dotfiles_config_content = scandir(&dotfiles_config_path);
   for file in dotfiles_config_content.files().iter() {
-    match symlink(file, &config_path) {
+    let file_name = Path::new(file).file_name().unwrap();   
+    let dest = String::from(
+      config_path.to_owned() + "/" + file_name.to_str().unwrap()
+    );
+    
+    match symlink(file, &dest) {
       Ok(_) => info!("file {} succefully linked to {}", file, config_path),
       Err(e) => error!("failed to link file {} to {}, due to: {}", file, config_path, e) 
     };
   }
 
   for dir in dotfiles_config_content.dirs().iter() {
-    match symlink(dir, &config_path) {
+    let file_name = Path::new(dir).file_name().unwrap();    
+    let dest = String::from(
+      config_path.to_owned() + "/" + file_name.to_str().unwrap()
+    );
+    
+    match symlink(dir, &dest) {
       Ok(_) => info!("dir {} succefully linked to {}", dir, config_path),
       Err(e) => error!("failed to link dir {} to {}, due to: {}", dir, config_path, e.to_string())
     }
