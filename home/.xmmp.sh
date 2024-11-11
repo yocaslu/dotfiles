4fb824a0eb1a10aab3a7c8cc25199dde18ffc786
olaLucas <lsantos2003silva@gmail.com>
2024-11-10 23:54:07 -0300

    Update

--- home/.xmmp.sh
+++ home/.xmmp.sh
@@ -6,15 +6,15 @@
 # xmodmap -e "keysym l = l L Right"  
 
 # Notebook Maps
+xmodmap -e "keycode 66 = Mode_switch" &&
+xmodmap -e "keysym j = j J Left" &&
+xmodmap -e "keysym k = k K Down" &&
+xmodmap -e "keysym l = l L Up" &&
+xmodmap -e "keysym ccedilla = ccedilla Ccedilla Right"
+
+# Desktop Maps 
 # xmodmap -e "keycode 66 = Mode_switch" &&
 # xmodmap -e "keysym j = j J Left" &&
 # xmodmap -e "keysym k = k K Down" &&
 # xmodmap -e "keysym l = l L Up" &&
 # xmodmap -e "keysym semicolon = semicolon colon  Right"
-
-# Desktop Maps 
-xmodmap -e "keycode 66 = Mode_switch" &&
-xmodmap -e "keysym j = j J Left" &&
-xmodmap -e "keysym k = k K Down" &&
-xmodmap -e "keysym l = l L Up" &&
-xmodmap -e "keysym semicolon = semicolon colon  Right"
