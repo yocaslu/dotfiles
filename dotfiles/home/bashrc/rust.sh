e48246a8e2b54a9bb2815a47e3cc20b4cb056266
olaLucas <lsantos2003silva@gmail.com>
2024-11-16 07:18:13 -0300

    adding cargo binaries to PATH

--- dotfiles/home/bashrc/rust.sh
+++ dotfiles/home/bashrc/rust.sh
@@ -1,1 +1,7 @@
-alias cg='cargo'
+# cargo install put the binaries here
+# so I need to add it to PATH
+# it may conflict with rustup tradicional installation
+# since i am using arch rustup package
+
+RUST_BIN_PATH="$HOME/.cargo/bin/"
+export PATH=$RUST_BIN_PATH:$PATH
