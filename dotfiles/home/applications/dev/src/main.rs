85e7a4e6efec04881c36ddd88d2d7d0466a2a217
olaLucas <lsantos2003silva@gmail.com>
2024-11-18 04:32:27 -0300

    organizing code in modules

--- dotfiles/home/applications/dev/src/main.rs
+++ dotfiles/home/applications/dev/src/main.rs
@@ -1,38 +1,16 @@
-use std::path::PathBuf;
-use std::process::exit;
-
-use log::warn;
-use log::info;
-use log::error;
-use clap::Parser;
-
 mod modules;
 use modules::cli;
 use modules::proc;
 use modules::tmux;
-use modules::logger;
-
-
 
 fn main() {
-  let args = cli::Args::parse();
-  let path = match args.path {
-    Some(p) => {
-      if p.exists() { p }
-      else {
-        error!("{} does not exist.", p.to_str().unwrap());
-        exit(-1);
-      }
-    },
-    
-    None => fzf()
-  };
+  let args = cli::parse();
   
-  if args.debug { logger::init_logger(); }
-  tmux::create(&path);
+  let session = tmux::Session::parse(args);
+  tmux::create_session(&session);
+  tmux::create_windows(&session);
 }
 
 // todo: check if an tmux session is already initiated, if so, create an diferent 
 // todo: check if all applications are installed
 // todo: create an config file to customize which applications will be started
-// todo: use fzf to get path
