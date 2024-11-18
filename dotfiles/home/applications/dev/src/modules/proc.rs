cdaab8a0110a46cef68fc447aed8694ea63589be
olaLucas <lsantos2003silva@gmail.com>
2024-11-18 04:31:11 -0300

    removing which from some functions

--- dotfiles/home/applications/dev/src/modules/proc.rs
+++ dotfiles/home/applications/dev/src/modules/proc.rs
@@ -1,24 +1,29 @@
+use std::io::Error;
+use std::io::ErrorKind;
 use std::process::Command;
 use std::process::Output;
 
+use log::info;
+use log::error;
+
 pub fn which(app_name: &str) -> bool {
-  match proc::execute("which", [app_name].to_vec()) {
+  match execute("which", [app_name].to_vec()) {
     Ok(_) => return true,
     Err(_) => return false
   };
 }
 
 // Create proc 
-pub fn execute(command: &str, args: Vec<&str>) -> Result<Output, io::Error> {
-  match Command::new(command).args(&args).output() {
-    Ok(o) => {
-      info!("succefully executed {} {:#?}", command, &args);
-      return Ok(o);
-    },
-  
-    Err(e) => {
-      error!("failed to execute {} {:#?}", command, &args);
-      return Err(e);
+pub fn execute(command: &str, args: Vec<&str>) -> Result<Output, Error> {
+    match Command::new(command).args(&args).output() {
+      Ok(o) => {
+        info!("succefully executed {} {:#?}", command, &args);
+        return Ok(o);
+      },
+    
+      Err(e) => {
+        error!("failed to execute {} {:#?}", command, &args);
+        return Err(e);
+      }
     }
-  };
 }
