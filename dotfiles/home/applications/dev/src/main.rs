c36489e7eff8709f777aac837ebdf53392f81222
olaLucas <lsantos2003silva@gmail.com>
2024-11-18 06:14:49 -0300

    adding "todos"

--- dotfiles/home/applications/dev/src/main.rs
+++ dotfiles/home/applications/dev/src/main.rs
@@ -5,12 +5,13 @@ use modules::tmux;
 
 fn main() {
   let args = cli::parse();
-  
   let session = tmux::Session::parse(args);
   tmux::create_session(&session);
   tmux::create_windows(&session);
 }
 
+// todo: generate more logs with more informations about the variables
+// todo: refactor proc, spawning more sophisticated process
 // todo: check if an tmux session is already initiated, if so, create an diferent 
 // todo: check if all applications are installed
 // todo: create an config file to customize which applications will be started
