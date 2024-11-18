9f2e5061080534a6ff9449b5c2061965a65d0297
olaLucas <lsantos2003silva@gmail.com>
2024-11-18 04:30:41 -0300

    adding pwd()

--- dotfiles/home/applications/dev/src/modules/env.rs
+++ dotfiles/home/applications/dev/src/modules/env.rs
@@ -1,2 +1,14 @@
-use std::env::var;
-use std::env::set_var;
+use std::env::current_dir;
+use std::path::PathBuf;
+use log::error;
+use std::process::exit;
+
+pub fn pwd() -> PathBuf {
+  match current_dir() {
+    Ok(p) => p,
+    Err(e) => {
+      error!("failed to get pwd: {}", e);
+      exit(-1); 
+    }
+  }
+}
