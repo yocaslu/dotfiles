1ef7f4143aff8e89b3bacf196873dc2491d1a563
olaLucas <lsantos2003silva@gmail.com>
2024-11-18 06:14:17 -0300

    adding some "todo"

--- dotfiles/home/applications/dev/src/modules/proc.rs
+++ dotfiles/home/applications/dev/src/modules/proc.rs
@@ -5,17 +5,17 @@ use std::process::Output;
 use log::info;
 use log::error;
 
-// Create proc 
+// todo: study more about process module to make it better
 pub fn execute(command: &str, args: Vec<&str>) -> Result<Output, Error> {
-    match Command::new(command).args(&args).output() {
-      Ok(o) => {
-        info!("succefully executed {} {:#?}", command, &args);
-        return Ok(o);
-      },
-    
-      Err(e) => {
-        error!("failed to execute {} {:#?}", command, &args);
-        return Err(e);
-      }
+  match Command::new(command).args(&args).output() {
+    Ok(o) => {
+      info!("succefully executed {} {:#?}", command, &args);
+      return Ok(o);
+    },
+  
+    Err(e) => {
+      error!("failed to execute {} {:#?}", command, &args);
+      return Err(e);
     }
+  }
 }
