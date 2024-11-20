493ffa87cb6c798e47bf7bc3d722044b9ee1b6c2
lucas <110935148+olaLucas@users.noreply.github.com>
2024-11-20 06:11:19 -0300

    refactoring execute

--- dotfiles/home/applications/dev/src/modules/proc.rs
+++ dotfiles/home/applications/dev/src/modules/proc.rs
@@ -1,20 +1,46 @@
 use std::io::Error;
 use std::process::Command;
 use std::process::Output;
+use std::process::Stdio;
+use std::path::PathBuf;
 
 use log::info;
 use log::error;
 
-// todo: study more about process module to make it better
-pub fn execute(command: &str, args: Vec<&str>) -> Result<Output, Error> {
-  match Command::new(command).args(&args).output() {
-    Ok(o) => {
-      info!("succefully executed {} {:#?}", command, &args);
-      return Ok(o);
+pub fn execute(command: &str, args: Vec<&str>, pwd: &PathBuf) -> Result<Output, Error> {
+  let proc = Command::new(command) 
+    .args(&args)
+    .current_dir(pwd)
+    .stdout(Stdio::piped())
+    .spawn();
+
+  match proc {
+    Ok(c) => {
+      info!("child process {}{:#?} spawned.", command, &args);
+      match c.wait_with_output() {
+        Ok(o) => {
+          info!(
+            "child process {}{:#?} exited with code {}.", 
+            command, args, o.status
+          );
+
+          return Ok(o);
+        },
+        
+        Err(e) => {
+          error!("child process {}{:#?} with error: {}",
+            command, args, e.to_string()
+          );
+
+          return Err(e);
+        }
+      }
     },
-  
+
     Err(e) => {
-      error!("failed to execute {} {:#?}", command, &args);
+      error!("could not spawm process {}{:#?}",
+        command, &args
+      );
       return Err(e);
     }
   }
