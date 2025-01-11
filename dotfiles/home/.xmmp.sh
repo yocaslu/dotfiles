7ed1c4e326130c04bf0a63878d766f14dd43706e
lucas <110935148+olaLucas@users.noreply.github.com>
2025-01-11 16:26:33 -0300

    removing git merge comments

--- dotfiles/home/.xmmp.sh
+++ dotfiles/home/.xmmp.sh
@@ -25,4 +25,3 @@ xmodmap -e "keysym h = h H Left" &&
 xmodmap -e "keysym j = j J Down" &&
 xmodmap -e "keysym k = k K Up" &&
 xmodmap -e "keysym l = l L Right"
->>>>>>> b28f6074ff123fdc47f7fbda035d1fa586241e35
