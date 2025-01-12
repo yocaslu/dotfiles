e1b7c592c371a438d4009c84ab186d30bf37e547
lucas <110935148+olaLucas@users.noreply.github.com>
2025-01-12 15:03:52 -0300

    major update

--- dotfiles/home/bashrc/terminal.sh
+++ dotfiles/home/bashrc/terminal.sh
@@ -1,9 +1,11 @@
 alias rc='source ~/.bashrc'
 alias cl='clear'
-alias ll='lsd -lha'
-alias ls='lsd -ha --color=auto'
+alias la='lsd -a'
+alias ls='lsd'
+alias ll='lsd -l'
+alias lla='lsd -la'
+alias lt='lsd --tree'
 alias grep='grep --color=auto'
 alias fetch='fastfetch -l arch2'
-alias tl="tree -lhagpf"
 alias xmmp="~/.xmmp.sh"
 alias syu="sudo pacman -Syu"
